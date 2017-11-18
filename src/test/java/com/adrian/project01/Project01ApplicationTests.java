f5f5a741aaa4e2c198d39dc32633b449231a6248
AMIMIM <anonimaf@wp.pl>
2017-11-18 18:06:05 +0100

    First change structure of the project.

--- src/test/java/com/adrian/project01/Project01ApplicationTests.java
+++ src/test/java/com/adrian/project01/Project01ApplicationTests.java
@@ -3,10 +3,12 @@ package com.adrian.project01;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = Project01Application.class)
+@SpringBootTest(classes = Project01Application.class)
 @WebAppConfiguration
 public class Project01ApplicationTests {
 
