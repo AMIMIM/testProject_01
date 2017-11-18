f05d33ccc46271938516c15e423ac70aec874fef
AMIMIM <anonimaf@wp.pl>
2017-11-18 13:54:50 +0100

    Znowu http://namiekko.pl/2016/03/13/git-przedzieram-sie-przez-galezie/

--- project01/src/test/java/com/adrian/project01/Project01ApplicationTests.java
+++ project01/src/test/java/com/adrian/project01/Project01ApplicationTests.java
@@ -5,8 +5,9 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Project01Application.class)
+@WebAppConfiguration
 public class Project01ApplicationTests {
 
 	@Test
